eae3920fbb9521e18d9d592d0b7fca3de7f9ce40
Erion Jasiqi <erion_jasiqi@sluz.ch>
2024-12-06 14:12:28 +0100

    completed tasks

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,15 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	firstName := "Erion"
+	lastName := "Jasiqi"
+	dayOfBirth := 4
+	monthOfBirth := 9
+	yearOfBirth := 2007
+	numberOfSiblings := 1
+	heightInMeters := 1.68
+	zodiacSign := '\u264D'
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
--- go-1-ex-3/main.go
+++ go-1-ex-3/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
@@ -11,11 +12,20 @@ func main() {
 	var when = time.Now()
 
 	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice shows", eyes, "eyes")
+	fmt.Fprintln(os.Stdout, "the dice shows", eyes, "eyes")
 
 	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice was rolled at", when)
+	fmt.Fprintln(os.Stdout, "the dice was rolled at", when)
 
 	// TODO: how to write the output into eyes.txt and dice.log?
+	eyesFile, _ := os.Create("eyes.txt") //ohne _ funktioniert es nicht, weiss nicht warum
+	fmt.Fprintln(eyesFile, "the dice shows", eyes, "eyes")
+	eyesFile.Close()
+
+	logFile, _ := os.Create("dice.log") //ohne _ funktioniert es nicht, weiss nicht warum
+	fmt.Fprintln(logFile, "the dice shows", eyes, "eyes")
+	fmt.Fprintln(logFile, "the dice was rolled at", when)
+	logFile.Close()
+
 	// go run ex3/main.go TODO
 }
